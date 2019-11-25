525accb82e27361cf0f567fe80b4c985abc5714d
natewolford <natenwolford@gmail.com>
2019-11-25 01:45:04 -0800

    Second commit

--- src/main/java/edu/gcccd/csis/Fibonacci.java
+++ src/main/java/edu/gcccd/csis/Fibonacci.java
@@ -2,34 +2,30 @@ package edu.gcccd.csis;
 import java.math.*;
 
 public class Fibonacci{
-    private static int counter = 0;
+    private static int timesRun;
 
-    public static final BigInteger[] fibMemory = new BigInteger[2001];
+    public static BigInteger fibonacci(BigInteger n, BigInteger t, int counter) {
+        timesRun++;
+        counter--;
 
-    static {
-        fibMemory[0] = BigInteger.ONE;
-        fibMemory[1] = BigInteger.ONE;
+        t = t.add(n);
+        n = t.subtract(n);
+
+        if (counter == 0) return t;
+        return fibonacci(n, t, counter);
+
     }
-
-    public static BigInteger fibonacci(int n) {
-        counter++;
-        if(fibMemory[n] == null)
-        if(n == 0) {
-            fibMemory[n] = BigInteger.valueOf(0);
-        } else if(n == 1 || n ==2) {
-            fibMemory[n]= BigInteger.valueOf(1);
-        } else {
-            fibMemory[n] = fibonacci(n-1).add(fibonacci(n-2));
-        }
-        return fibMemory[n];
+    public static BigInteger fibonacci(int counter) {
+        counter--;
+        return fibonacci(BigInteger.ZERO, BigInteger.ONE, counter);
     }
 
     public static void main(String[] args) {
         long time, newTime;
         time = System.currentTimeMillis();
-        System.out.println("The 2000-th Fibonacci Number is " + fibonacci(2000) );
+        System.out.println("The 2000-th Fibonacci Number is " + fibonacci(2000));
         newTime = System.currentTimeMillis();
         System.out.println("and was calculated in " + (newTime - time) + " ms" );
-        System.out.println("The fibonacci() method was called " + counter + " times");
+        System.out.println("The fibonacci() method was called " + timesRun + " times");
     }
 }
--- src/test/java/edu/gcccd/csis/FibonacciTest.java
+++ src/test/java/edu/gcccd/csis/FibonacciTest.java
@@ -18,7 +18,6 @@ public class FibonacciTest {
 
 
     }
-
     @Test
     public void main() {
 
